947db1128a978742d98b721faa6e97f344e8cd78
jasondoan <doananhtuan22111996@gmail.com>
2024-11-18 00:01:52 +0700

    feat: migrate to Kotlin 2xx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.androidHilt) apply false
+    alias(mobilex.plugins.composeCompiler) apply false
+    alias(mobilex.plugins.androidHilt) apply false
 }
